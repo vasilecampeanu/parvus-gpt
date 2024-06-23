9f1ae4bd9feeffbd1226ebaa11880cb5601bc537
vasilecampeanu <v.campeanu@topmotive.eu>
2024-06-23 12:31:08 +0300

    feat: Added GPU checks for cuda and mps

--- gpt-2/wanna_train.py
+++ gpt-2/wanna_train.py
@@ -3,10 +3,20 @@ from awesome_gpt import GPT, GPTConfig
 from data_loader_lite import DataLoaderLite
 import time
 
-device = 'mps'
+device = "mps"
+
+torch.manual_seed(1337)
+
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    torch.mps.manual_seed(1337)
+
+train_loader = DataLoaderLite(B=4, T=1024)
+torch.set_float32_matmul_precision('high')
+
 model = GPT(GPTConfig())
 model.to(device)
-train_loader = DataLoaderLite(B=4, T=32)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 
@@ -21,11 +31,17 @@ for i in range(50):
     loss.backward()
     optimizer.step()
 
-    torch.mps.synchronize() # Wait for the GPU to finish work
+    # Wait for the GPU to finish work
+
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+        torch.mps.synchronize()
 
     t1 = time.time()
 
-    dt = (t1 - t0) * 1000 # Time difference in miliseconds
+    # Time difference in miliseconds
+    dt = (t1 - t0) * 1000
     tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
 
-    print(f"loss at step {i:02} | loss: {loss.item():18.15f} | dt: {dt:6.2f}ms | tok/sec: {tokens_per_sec:6.2f}")
+    print(f"loss at step {i:02} | loss: {loss.item():18.15f} | dt: {dt:8.2f}ms | tok/sec: {tokens_per_sec:6.2f}")
