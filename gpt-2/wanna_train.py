6f548ca95ccc8cdbcfa52d9ce30749bd25388885
vasilecampeanu <v.campeanu@topmotive.eu>
2024-07-12 12:38:38 +0300

    feat: Added assets

--- gpt-2/wanna_train.py
+++ gpt-2/wanna_train.py
@@ -40,8 +40,10 @@ with open(f'trainlog/training_log_{device}.csv', mode='w', newline='') as file:
         x, y = x.to(device), y.to(device)
 
         optimizer.zero_grad()
+
         with torch.autocast(device_type=device, dtype=torch.bfloat16):
             logits, loss = model(x, y)
+
         loss.backward()
         optimizer.step()
 
